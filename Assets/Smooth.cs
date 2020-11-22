36d589603c0f168c87d9b35ea44ea1edf428bad6
Cong Wang <wangcongrobot@gmail.com>
2020-11-22 13:36:17 +0000

    add testing videos

---
+++ Assets/Smooth.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Pathfinding;
+
+public class Smooth : MonoModifier {
+    public override int Order { get { return 60; } }
+
+    public int iterations = 5;
+
+    public int subdivisions = 2;
+
+    public override void Apply (Path path) {
+        if (path.error || path.vectorPath == null || path.vectorPath.Count <= 2) {
+            return;
+        }
+        // Subdivisions should not be less than zero
+        subdivisions = Mathf.Max(subdivisions, 0);
+
+        // Prevent unknowing users from entering bad values
+        if (subdivisions > 12) {
+            Debug.LogWarning("Subdividing a path more than 12 times is quite a lot, it might cause memory problems and it will certainly slow the game down.\n" +
+                "When this message is logged, no smoothing will be applied");
+            subdivisions = 12;
+            return;
+        }
+
+        // Create a new list to hold the smoothed path
+        List<Vector3> newPath = new List<Vector3>();
+        List<Vector3> originalPath = path.vectorPath;
+
+        // One segment (line) in the original array will be subdivided to this number of smaller segments
+        int subSegments = (int)Mathf.Pow(2, subdivisions);
+        float fractionPerSegment = 1F / subSegments;
+        for (int i = 0; i < originalPath.Count - 1; i++) {
+            for (int j = 0; j < subSegments; j++) {
+                // Use Vector3.Lerp to place the points at their correct positions along the line
+                newPath.Add(Vector3.Lerp(originalPath[i], originalPath[i+1], j*fractionPerSegment));
+            }
+        }
+
+        // Add the last point
+        newPath.Add(originalPath[originalPath.Count-1]);
+
+        // Smooth the path [iterations] number of times
+        for (int it = 0; it < iterations; it++) {
+            // Loop through all points except the first and the last
+            for (int i = 1; i < newPath.Count-1; i++) {
+                // Set the new point to the average of the current point and the two adjacent points
+                Vector3 newpoint = (newPath[i] + newPath[i-1] + newPath[i+1]) / 3F;
+                newPath[i] = newpoint;
+            }
+        }
+
+        // Assign the new path to the p.vectorPath field
+        path.vectorPath = newPath;
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Unity Pathfinding Project
 
-# Unity
+## Unity
 
 unity version: 2018.4.28f1
 
-# project
+## project
 
 Assets/unity_pathfinding/ExampleScenes/unity_pathfinding
 
@@ -14,3 +14,74 @@ Assets/unity_pathfinding/Core/AI/Seeker.cs
 
 
 我们将使用Unity内置组件[CharacterController](https://docs.unity3d.com/ScriptReference/CharacterController.html)移动agent。因此，将一个CharacterController附加到AI GameObject。
+
+## Testing
+
+- Heuristic: Manhattan
+
+Path Completed : Computation Time 26.398 ms Searched Nodes 1090 Path Length 89
+End Node
+	G: 247972
+	H: 2500
+	F: 250472
+	Point: (200.0, 4.9, 350.0)
+	Graph: 0
+Start Node
+	Point: (133.4, 0.0, 131.3)
+	Graph: 0
+Path Number 1 (unique id)
+
+![](videos/unity_pathfinding_Manhattan.mp4)
+
+
+- Heuristic: Diagonal Manhattan
+
+Path Completed : Computation Time 26.486 ms Searched Nodes 1411 Path Length 89
+End Node
+	G: 247972
+	H: 1750
+	F: 249722
+	Point: (200.0, 4.9, 350.0)
+	Graph: 0
+Start Node
+	Point: (133.4, 0.0, 131.3)
+	Graph: 0
+Path Number 1 (unique id)
+
+![](videos/unity_pathfinding_Diagonal_Manhattan.mp4)
+
+- Heuristic: Euclidean
+
+Path Completed : Computation Time 30.688 ms Searched Nodes 2432 Path Length 89
+End Node
+	G: 247972
+	H: 1768
+	F: 249740
+	Point: (200.0, 4.9, 350.0)
+	Graph: 0
+Start Node
+	Point: (133.4, 0.0, 131.3)
+	Graph: 0
+Path Number 1 (unique id)
+
+![](videos/unity_pathfinding_Euclidean.mp4)
+
+- Heuristic: None
+
+Path Completed : Computation Time 61.724 ms Searched Nodes 18247 Path Length 89
+End Node
+	G: 247972
+	H: 0
+	F: 247972
+	Point: (200.0, 4.9, 350.0)
+	Graph: 0
+Start Node
+	Point: (133.4, 0.0, 131.3)
+	Graph: 0
+Path Number 1 (unique id)
+
+![](videos/unity_pathfinding_None.mp4)
+
+- Multi-Agents
+
+![](videos/unity_pathfinding_Multi_agents.mp4)
